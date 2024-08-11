ab80345638fbae52780a2e48c949c4c4bad7af7b
Prytula Dmytro Vitaliyovych <dimamon02@gmail.com>
2024-08-11 12:04:31 +0300

    done

--- dz18_06/MainWindow.xaml.cs
+++ dz18_06/MainWindow.xaml.cs
@@ -1,1 +1,111 @@
-﻿
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace system7
+
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<HorseViewModel> horses;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public ObservableCollection<HorseViewModel> Horses
+        {
+            get { return horses; }
+            set { horses = value; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            InitializeHorses();
+        }
+
+        private void InitializeHorses()
+        {
+            Horses = new ObservableCollection<HorseViewModel>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Horses.Add(new HorseViewModel { Name = "Horse " + i, Distance = 0 });
+            }
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                await Task.Run(() => StartRace(cancellationTokenSource.Token));
+            }
+            catch (TaskCanceledException)
+            {
+
+            }
+        }
+
+        public class HorseViewModel : INotifyPropertyChanged
+        {
+            private string name;
+            public string Name
+            {
+                get { return name; }
+                set
+                {
+                    name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+
+            private int distance;
+            public int Distance
+            {
+                get { return distance; }
+                set
+                {
+                    distance = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Distance"));
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
+        private async Task StartRace(CancellationToken cancellationToken)
+        {
+            Random random = new Random();
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(500);
+
+                foreach (var horse in Horses)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                        break;
+
+                    horse.Distance += random.Next(1, 11);
+                }
+
+                foreach (var horse in Horses)
+                {
+                    if (horse.Distance >= 1000)
+                    {
+                        MessageBox.Show($"{horse.Name} wins the race!", "Race Over", MessageBoxButton.OK, MessageBoxImage.Information);
+                        cancellationTokenSource.Cancel();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+
+}
